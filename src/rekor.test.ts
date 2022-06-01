d7c8fdd29a43c84f5ac57e9dc70785ac5bbd9c14
Brian DeHamer <brian@dehamer.com>
2022-06-01 10:53:08 -0700

    add rekor api client

---
+++ src/rekor.test.ts
@@ -0,0 +1,179 @@
+import { ProposedEntry, Rekor } from './rekor';
+import nock from 'nock';
+
+describe('Rekor', () => {
+  const baseURL = 'http://localhost:8080';
+  const subject = new Rekor({ baseURL });
+
+  it('should create an instance', () => {
+    expect(subject).toBeTruthy();
+  });
+
+  describe('#createEntry', () => {
+    const proposedEntry: ProposedEntry = {
+      artifactSignature:
+        'MEUCIDB2SWDabztSC8RrlfRCWUf04LBN0E2CEwiDZJLacDS8AiEA3bQHMBpodxA3dvJ+JK1SALkuzju/w4oCg3S89c8CtN8=',
+      artifactDigest:
+        '1c025a6e48ceb8bf10e01b367089732326eabe3541d03d348724c79040382c65',
+      certificate:
+        'LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCi0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K',
+    };
+
+    describe('when the entry is successfully added', () => {
+      const uuid =
+        '69e5a0c1663ee4452674a5c9d5050d866c2ee31e2faaf79913aea7cc27293cf6';
+      const responseBody = {
+        [uuid]: {
+          body: 'Zm9vCg==',
+          integratedTime: 1654015743,
+          logID:
+            'c0d23d6ad406973f9559f3ba2d1ca01f84147d8ffc5b8445c224f98b9591801d',
+          logIndex: 2513258,
+          verification: {
+            signedEntryTimestamp:
+              'MEUCIQD6CD7ZNLUipFoxzmSL/L8Ewic4SRkXN77UjfJZ7d/wAAIgatokSuX9Rg0iWxAgSfHMtcsagtDCQalU5IvXdQ+yLEA=',
+          },
+        },
+      };
+
+      beforeEach(() => {
+        nock(baseURL).post('/api/v1/log/entries').reply(201, responseBody);
+      });
+
+      it('returns the new entry', async () => {
+        const result = await subject.createEntry(proposedEntry);
+
+        expect(result.uuid).toBe(uuid);
+        expect(result.body).toBe(responseBody[uuid].body);
+        expect(result.integratedTime).toBe(responseBody[uuid].integratedTime);
+        expect(result.logID).toBe(responseBody[uuid].logID);
+        expect(result.logIndex).toBe(responseBody[uuid].logIndex);
+        expect(result.verification).toEqual(responseBody[uuid].verification);
+      });
+    });
+
+    describe('when a matching entry already exists', () => {
+      const responseBody = {
+        code: 409,
+        message: 'An equivalent entry already exists',
+      };
+
+      beforeEach(() => {
+        nock(baseURL).post('/api/v1/log/entries').reply(409, responseBody);
+      });
+
+      it('returns an error', async () => {
+        await expect(subject.createEntry(proposedEntry)).rejects.toThrow(
+          'HTTP Error: 409 Conflict'
+        );
+      });
+    });
+  });
+
+  describe('#getEntry', () => {
+    describe('when the entry exists', () => {
+      const uuid =
+        '69e5a0c1663ee4452674a5c9d5050d866c2ee31e2faaf79913aea7cc27293cf6';
+      const responseBody = {
+        [uuid]: {
+          body: 'Zm9vCg==',
+          integratedTime: 1654015743,
+          logID:
+            'c0d23d6ad406973f9559f3ba2d1ca01f84147d8ffc5b8445c224f98b9591801d',
+          logIndex: 2513258,
+          verification: {
+            signedEntryTimestamp:
+              'MEUCIQD6CD7ZNLUipFoxzmSL/L8Ewic4SRkXN77UjfJZ7d/wAAIgatokSuX9Rg0iWxAgSfHMtcsagtDCQalU5IvXdQ+yLEA=',
+          },
+        },
+      };
+
+      beforeEach(() => {
+        nock(baseURL)
+          .get(`/api/v1/log/entries/${uuid}`)
+          .reply(200, responseBody);
+      });
+
+      it('returns the requested entry', async () => {
+        const result = await subject.getEntry(uuid);
+
+        expect(result.uuid).toBe(uuid);
+        expect(result.body).toBe(responseBody[uuid].body);
+        expect(result.integratedTime).toBe(responseBody[uuid].integratedTime);
+        expect(result.logID).toBe(responseBody[uuid].logID);
+        expect(result.logIndex).toBe(responseBody[uuid].logIndex);
+        expect(result.verification).toEqual(responseBody[uuid].verification);
+      });
+    });
+
+    describe('when the entry does not exist', () => {
+      const responseBody = {
+        code: 404,
+        message: 'Entry not found',
+      };
+
+      beforeEach(() => {
+        nock(baseURL).get('/api/v1/log/entries/foo').reply(404, responseBody);
+      });
+
+      it('returns an error', async () => {
+        await expect(subject.getEntry('foo')).rejects.toThrow(
+          'HTTP Error: 404 Not Found'
+        );
+      });
+    });
+  });
+
+  describe('#search', () => {
+    describe('when matching entries exist', () => {
+      const responseBody = ['deadbeef', 'abcd1234'];
+
+      beforeEach(() => {
+        nock(baseURL).post('/api/v1/index/retrieve').reply(200, responseBody);
+      });
+
+      it('returns matching entries', async () => {
+        const sha =
+          'sha256:04c0c13721a28c60f38daf09a05326c301a2cf57ad2beb953eb29d61383db47e';
+
+        const response = await subject.searchLog({ hash: sha });
+
+        expect(response).toEqual(responseBody);
+      });
+    });
+
+    describe('when no matching entries exist', () => {
+      const responseBody = [] as string[];
+
+      beforeEach(() => {
+        nock(baseURL).post('/api/v1/index/retrieve').reply(200, responseBody);
+      });
+
+      it('returns an empty array', async () => {
+        const sha =
+          'sha256:04c0c13721a28c60f38daf09a05326c301a2cf57ad2beb953eb29d61383db47e';
+
+        const response = await subject.searchLog({ hash: sha });
+
+        expect(response).toEqual([]);
+      });
+    });
+
+    describe('when an error occurs', () => {
+      const responseBody = {
+        code: 422,
+        message: 'Invalid query',
+      };
+
+      beforeEach(() => {
+        nock(baseURL).get('/api/v1/log/entries/foo').reply(422, responseBody);
+      });
+
+      it('returns an error', async () => {
+        await expect(subject.getEntry('foo')).rejects.toThrow(
+          'HTTP Error: 422 Unprocessable Entity'
+        );
+      });
+    });
+  });
+});
---
+++ src/rekor.ts
@@ -0,0 +1,127 @@
+import fetch, { FetchInterface } from 'make-fetch-happen';
+import { checkStatus } from './error';
+
+const DEFAULT_BASE_URL = 'https://rekor.sigstore.dev';
+const ENTRY_KIND = 'hashedrekord';
+const API_VERSION = '0.0.1';
+
+// Client options
+export interface RekorOptions {
+  baseURL?: string;
+}
+
+export interface ProposedEntry {
+  artifactSignature: string;
+  artifactDigest: string;
+  certificate: string;
+}
+
+export interface Entry {
+  uuid: string;
+  body: string;
+  integratedTime: number;
+  logID: string;
+  logIndex: number;
+  verification: object;
+  attestation?: object;
+}
+
+export interface SearchIndex {
+  email?: string;
+  hash?: string;
+}
+
+/**
+ * Rekor API client.
+ */
+export class Rekor {
+  private fetch: FetchInterface;
+
+  private baseUrl: string;
+  constructor(options: RekorOptions) {
+    this.fetch = fetch.defaults({
+      retry: { retries: 2 },
+      timeout: 5000,
+      headers: { Accept: 'application/json' },
+    });
+    this.baseUrl = options.baseURL ?? DEFAULT_BASE_URL;
+  }
+
+  /**
+   * Create a new entry in the Rekor log.
+   * @param propsedEntry {ProposedEntry} Data to create a new entry
+   * @returns {Promise<Entry>} The created entry
+   */
+  public async createEntry(propsedEntry: ProposedEntry): Promise<Entry> {
+    const url = `${this.baseUrl}/api/v1/log/entries`;
+
+    const body = {
+      kind: ENTRY_KIND,
+      apiVersion: API_VERSION,
+      spec: {
+        data: {
+          hash: { algorithm: 'sha256', value: propsedEntry.artifactDigest },
+        },
+        signature: {
+          format: 'x509',
+          content: propsedEntry.artifactSignature,
+          publicKey: { content: propsedEntry.certificate },
+        },
+      },
+    };
+
+    const response = await this.fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+    });
+    checkStatus(response);
+
+    const data = await response.json();
+    return entryFromResponse(data);
+  }
+
+  /**
+   * Get an entry from the Rekor log.
+   * @param uuid {string} The UUID of the entry to retrieve
+   * @returns {Promise<Entry>} The retrieved entry
+   */
+  public async getEntry(uuid: string): Promise<Entry> {
+    const url = `${this.baseUrl}/api/v1/log/entries/${uuid}`;
+
+    const response = await fetch(url);
+    checkStatus(response);
+
+    const data = await response.json();
+    return entryFromResponse(data);
+  }
+
+  /**
+   * Search the Rekor log for entries matching the given query.
+   * @param opts {SearchIndex} Options to search the Rekor log
+   * @returns {Promise<string[]>} UUIDs of matching entries
+   */
+  public async searchLog(opts: SearchIndex): Promise<string[]> {
+    const url = `${this.baseUrl}/api/v1/index/retrieve`;
+
+    const response = await this.fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(opts),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    checkStatus(response);
+
+    const data = await response.json();
+    return data;
+  }
+}
+
+// Unpack the response from the Rekor API into a more convenient format.
+function entryFromResponse(data: object): Entry {
+  // Grab UUID and entry data from the response
+  const [uuid, entry] = Object.entries(data)[0];
+
+  return {
+    ...(entry as Entry),
+    uuid,
+  };
+}
