c13d8784ac25d3c90d7637dadf2471ebc4c6a308
Brian DeHamer <brian@dehamer.com>
2022-06-03 14:56:17 -0700

    fixup recor client

--- src/rekor.ts
+++ src/rekor.ts
@@ -22,8 +22,20 @@ export interface Entry {
   integratedTime: number;
   logID: string;
   logIndex: number;
-  verification: object;
+  verification: EntryVerification;
   attestation?: object;
+}
+
+export interface EntryVerification {
+  inclusionProof: InclusionProof;
+  signedEntryTimestamp: string;
+}
+
+export interface InclusionProof {
+  hashes: string[];
+  logIndex: number;
+  rootHash: string;
+  treeSize: number;
 }
 
 export interface SearchIndex {
@@ -72,6 +84,7 @@ export class Rekor {
 
     const response = await this.fetch(url, {
       method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
     });
     checkStatus(response);
@@ -88,7 +101,7 @@ export class Rekor {
   public async getEntry(uuid: string): Promise<Entry> {
     const url = `${this.baseUrl}/api/v1/log/entries/${uuid}`;
 
-    const response = await fetch(url);
+    const response = await this.fetch(url);
     checkStatus(response);
 
     const data = await response.json();
@@ -117,6 +130,12 @@ export class Rekor {
 
 // Unpack the response from the Rekor API into a more convenient format.
 function entryFromResponse(data: object): Entry {
+  const entries = Object.entries(data);
+
+  if (entries.length != 1) {
+    throw new Error('Received multiple entries in Rekor response');
+  }
+
   // Grab UUID and entry data from the response
   const [uuid, entry] = Object.entries(data)[0];
 
