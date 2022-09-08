e86021122dfc136ba483991f96af1bd937a29c21
Brian DeHamer <bdehamer@github.com>
2022-09-07 17:29:43 -0700

    prep for publishing alpha package
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+export {
+  SigstoreBlobBundle,
+  SigstoreBundle,
+  SigstoreDSSEBundle,
+} from './bundle';
 export * as sigstore from './sigstore';
