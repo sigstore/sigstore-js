efdfa53b4693bee1593899cee6d965ed69ea13ab
Brian DeHamer <bdehamer@github.com>
2022-12-07 09:59:08 -0800

    x509 cert parsing (#187)
    Signed-off-by: Brian DeHamer <bdehamer@github.com>
    
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

---
+++ src/x509/cert.ts
@@ -0,0 +1,164 @@
+import crypto from 'crypto';
+import { ASN1Obj } from './asn1/obj';
+import {
+  x509BasicConstraintsExtension,
+  x509KeyUsageExtension,
+  x509SCTExtension,
+} from './ext';
+
+const EXTENSION_OID_KEY_USAGE = '2.5.29.15';
+const EXTENSION_OID_BASIC_CONSTRAINTS = '2.5.29.19';
+const EXTENSION_OID_SCT = '1.3.6.1.4.1.11129.2.4.2';
+
+const ECDSA_SIGNATURE_ALGOS: Record<string, string> = {
+  '1.2.840.10045.4.3.1': 'sha224',
+  '1.2.840.10045.4.3.2': 'sha256',
+  '1.2.840.10045.4.3.3': 'sha384',
+  '1.2.840.10045.4.3.4': 'sha512',
+};
+
+export class x509Certificate {
+  public root: ASN1Obj;
+
+  constructor(asn1: ASN1Obj) {
+    this.root = asn1;
+  }
+
+  public static fromDER(der: Buffer): x509Certificate {
+    const asn1 = ASN1Obj.parseBuffer(der);
+    return new x509Certificate(asn1);
+  }
+
+  get tbsCertificate(): Buffer {
+    return this.tbsCertificateObj.raw;
+  }
+
+  get version(): string {
+    // version number is the first element of the version context specific tag
+    const ver = this.versionObj.subs[0].toInteger();
+    return `v${(ver + BigInt(1)).toString()}`;
+  }
+
+  get notBefore(): Date {
+    // notBefore is the first element of the validity sequence
+    return this.validityObj.subs[0].toDate();
+  }
+
+  get notAfter(): Date {
+    // notAfter is the second element of the validity sequence
+    return this.validityObj.subs[1].toDate();
+  }
+
+  get issuer(): Buffer {
+    return this.issuerObj.value;
+  }
+
+  get subject(): Buffer {
+    return this.subjectObj.value;
+  }
+
+  get publicKey(): crypto.KeyObject {
+    return crypto.createPublicKey({
+      key: this.subjectPublicKeyInfoObj.raw,
+      format: 'der',
+      type: 'spki',
+    });
+  }
+
+  get signatureAlgorithm(): string {
+    const oid: string = this.signatureAlgorithmObj.subs[0].toOID();
+    return ECDSA_SIGNATURE_ALGOS[oid] || '';
+  }
+
+  get signatureValue(): Buffer {
+    // Signature value is a bit string, so we need to skip the first byte
+    return this.signatureValueObj.value.subarray(1);
+  }
+
+  get extKeyUsage(): x509KeyUsageExtension | undefined {
+    const ext = this.findExtension(EXTENSION_OID_KEY_USAGE);
+    return ext ? new x509KeyUsageExtension(ext) : undefined;
+  }
+
+  get extBasicConstraints(): x509BasicConstraintsExtension | undefined {
+    const ext = this.findExtension(EXTENSION_OID_BASIC_CONSTRAINTS);
+    return ext ? new x509BasicConstraintsExtension(ext) : undefined;
+  }
+
+  get extSCT(): x509SCTExtension | undefined {
+    const ext = this.findExtension(EXTENSION_OID_SCT);
+    return ext ? new x509SCTExtension(ext) : undefined;
+  }
+
+  private findExtension(oid: string): ASN1Obj | undefined {
+    // The extension list is the first (and only) element of the extensions
+    // context specific tag
+    const extSeq = this.extensionsObj?.subs[0];
+
+    // Find the extension with the given OID. The OID will always be the first
+    // element of the extension sequence
+    return extSeq?.subs.find((ext) => ext.subs[0].toOID() === oid);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // The following properties use the documented x509 structure to locate the
+  // desired ASN.1 object
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1
+
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.1.1
+  private get tbsCertificateObj(): ASN1Obj {
+    // tbsCertificate is the first element of the certificate sequence
+    return this.root.subs[0];
+  }
+
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.1.2
+  private get signatureAlgorithmObj(): ASN1Obj {
+    // signatureAlgorithm is the second element of the certificate sequence
+    return this.root.subs[1];
+  }
+
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.1.3
+  private get signatureValueObj(): ASN1Obj {
+    // signatureValue is the third element of the certificate sequence
+    return this.root.subs[2];
+  }
+
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.2.1
+  private get versionObj(): ASN1Obj {
+    // version is the first element of the tbsCertificate sequence
+    return this.tbsCertificateObj.subs[0];
+  }
+
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.2.4
+  private get issuerObj(): ASN1Obj {
+    // issuer is the fourth element of the tbsCertificate sequence
+    return this.tbsCertificateObj.subs[3];
+  }
+
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.2.5
+  private get validityObj(): ASN1Obj {
+    // version is the fifth element of the tbsCertificate sequence
+    return this.tbsCertificateObj.subs[4];
+  }
+
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.2.6
+  private get subjectObj(): ASN1Obj {
+    // subject is the sixth element of the tbsCertificate sequence
+    return this.tbsCertificateObj.subs[5];
+  }
+
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.2.7
+  private get subjectPublicKeyInfoObj(): ASN1Obj {
+    // subjectPublicKeyInfo is the seventh element of the tbsCertificate sequence
+    return this.tbsCertificateObj.subs[6];
+  }
+
+  // Extensions can't be located by index because their position varies. Instead,
+  // we need to find the extensions context specific tag
+  // https://www.rfc-editor.org/rfc/rfc5280#section-4.1.2.9
+  private get extensionsObj(): ASN1Obj | undefined {
+    return this.tbsCertificateObj.subs.find((sub) =>
+      sub.tag.isContextSpecific(0x03)
+    );
+  }
+}
---
+++ src/x509/ext.ts
@@ -0,0 +1,82 @@
+import { ASN1Obj } from './asn1/obj';
+
+// https://www.rfc-editor.org/rfc/rfc5280#section-4.1
+class x509Extension {
+  protected root: ASN1Obj;
+
+  constructor(asn1: ASN1Obj) {
+    this.root = asn1;
+  }
+
+  get oid(): string {
+    return this.root.subs[0].toOID();
+  }
+
+  get critical(): boolean {
+    // The critical field is optional and will be the second element of the
+    // extension sequence if present. Default to false if not present.
+    return this.root.subs.length === 3 ? this.root.subs[1].toBoolean() : false;
+  }
+
+  protected get extnValueObj(): ASN1Obj {
+    // The extnValue field will be the last element of the extension sequence
+    return this.root.subs[this.root.subs.length - 1];
+  }
+}
+
+// https://www.rfc-editor.org/rfc/rfc5280#section-4.2.1.9
+export class x509BasicConstraintsExtension extends x509Extension {
+  constructor(asn1: ASN1Obj) {
+    super(asn1);
+  }
+
+  get isCA(): boolean {
+    return this.sequence.subs[0].toBoolean();
+  }
+
+  get pathLenConstraint(): bigint | undefined {
+    return this.sequence.subs.length > 1
+      ? this.sequence.subs[1].toInteger()
+      : undefined;
+  }
+
+  // The extnValue field contains a single sequence wrapping the isCA and
+  // pathLenConstraint.
+  private get sequence(): ASN1Obj {
+    return this.extnValueObj.subs[0];
+  }
+}
+
+// https://www.rfc-editor.org/rfc/rfc5280#section-4.2.1.3
+export class x509KeyUsageExtension extends x509Extension {
+  constructor(asn1: ASN1Obj) {
+    super(asn1);
+  }
+
+  get digitalSignature(): boolean {
+    return this.bitString[0] === 1;
+  }
+
+  get keyCertSign(): boolean {
+    return this.bitString[5] === 1;
+  }
+
+  get crlSign(): boolean {
+    return this.bitString[6] === 1;
+  }
+
+  // The extnValue field contains a single bit string which is a bit mask
+  // indicating which key usages are enabled.
+  private get bitString(): number[] {
+    return this.extnValueObj.subs[0].toBitString();
+  }
+}
+
+// https://www.rfc-editor.org/rfc/rfc6962#section-3.3
+export class x509SCTExtension extends x509Extension {
+  constructor(asn1: ASN1Obj) {
+    super(asn1);
+  }
+
+  // TODO: Parse the SCTs
+}
