9c1cb229dc607c665aec22c9ca5ecb698da0c532
Brian DeHamer <bdehamer@github.com>
2023-04-20 07:01:33 -0700

    integrate shoehorn for better testing type safety (#419)
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

--- src/__tests__/identity/oauth.test.ts
+++ src/__tests__/identity/oauth.test.ts
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+import { fromPartial } from '@total-typescript/shoehorn';
 import child_process, { ChildProcess } from 'child_process';
 import fetch from 'make-fetch-happen';
 import nock from 'nock';
@@ -117,6 +118,6 @@ function sendFakeAuthResponse(
       throw new Error('Invalid auth request');
     }
 
-    return {} as unknown as ChildProcess;
+    return fromPartial({});
   };
 }
