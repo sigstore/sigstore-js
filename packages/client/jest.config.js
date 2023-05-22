34912914b7748b226b75a76b4389bdc68c95331d
Brian DeHamer <bdehamer@github.com>
2023-05-22 14:05:03 -0700

    move jest helpers back to jest.setup
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -18,7 +18,7 @@ const base = require('../../jest.config.base')
 module.exports = {
   ...base,
   displayName: 'client',
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/dist/',
     '<rootDir>/src/__tests__/__fixtures__',
