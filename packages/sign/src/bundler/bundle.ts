84eee0b7e425e891c4c4ec2f04d7e28ccc861aa4
Brian DeHamer <bdehamer@github.com>
2023-07-26 07:07:22 -0700

    move bundle construction helpers to bundle pkg (#636)
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

--- packages/sign/src/bundler/bundle.ts
+++ packages/sign/src/bundler/bundle.ts
@@ -13,12 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { BUNDLE_V01_MEDIA_TYPE } from '@sigstore/bundle';
-import { HashAlgorithm } from '@sigstore/protobuf-specs';
+import * as sigstore from '@sigstore/bundle';
 import { crypto, pem } from '../util';
 
-import type * as sigstore from '@sigstore/bundle';
-import type { KeyMaterial, Signature } from '../signer';
+import type { Signature } from '../signer';
 import type { Artifact } from './base';
 
 // Helper functions for assembling the parts of a Sigstore bundle
@@ -30,20 +28,16 @@ export function toMessageSignatureBundle(
 ): sigstore.BundleWithMessageSignature {
   const digest = crypto.hash(artifact.data);
 
-  return {
-    mediaType: BUNDLE_V01_MEDIA_TYPE,
-    content: {
-      $case: 'messageSignature',
-      messageSignature: {
-        messageDigest: {
-          algorithm: HashAlgorithm.SHA2_256,
-          digest: digest,
-        },
-        signature: signature.signature,
-      },
-    },
-    verificationMaterial: toVerificationMaterial(signature.key),
-  };
+  return sigstore.toMessageSignatureBundle({
+    digest,
+    signature: signature.signature,
+    certificate:
+      signature.key.$case === 'x509Certificate'
+        ? pem.toDER(signature.key.certificate)
+        : undefined,
+    keyHint:
+      signature.key.$case === 'publicKey' ? signature.key.hint : undefined,
+  });
 }
 
 // DSSE envelope bundle - $case: 'dsseEnvelope'
@@ -51,72 +45,15 @@ export function toDSSEBundle(
   artifact: Required<Artifact>,
   signature: Signature
 ): sigstore.BundleWithDsseEnvelope {
-  return {
-    mediaType: BUNDLE_V01_MEDIA_TYPE,
-    content: {
-      $case: 'dsseEnvelope',
-      dsseEnvelope: toEnvelope(artifact, signature),
-    },
-    verificationMaterial: toVerificationMaterial(signature.key),
-  };
-}
-
-function toEnvelope(
-  artifact: Required<Artifact>,
-  signature: Signature
-): sigstore.Envelope {
-  return {
-    payloadType: artifact.type,
-    payload: artifact.data,
-    signatures: [toSignature(signature)],
-  };
-}
-
-function toSignature(signature: Signature): sigstore.Signature {
-  return {
-    keyid: toKeyID(signature),
-    sig: signature.signature,
-  };
-}
-
-function toKeyID(signature: Signature): string {
-  switch (signature.key.$case) {
-    case 'publicKey':
-      return signature.key.hint || '';
-    case 'x509Certificate':
-      return '';
-  }
-}
-
-// Verification material
-
-function toVerificationMaterial(
-  key: KeyMaterial
-): sigstore.Bundle['verificationMaterial'] {
-  return {
-    content: toKeyContent(key),
-    tlogEntries: [],
-    timestampVerificationData: { rfc3161Timestamps: [] },
-  };
-}
-
-function toKeyContent(
-  key: KeyMaterial
-): sigstore.Bundle['verificationMaterial']['content'] {
-  switch (key.$case) {
-    case 'publicKey':
-      return {
-        $case: 'publicKey',
-        publicKey: {
-          hint: key.hint || '',
-        },
-      };
-    case 'x509Certificate':
-      return {
-        $case: 'x509CertificateChain',
-        x509CertificateChain: {
-          certificates: [{ rawBytes: pem.toDER(key.certificate) }],
-        },
-      };
-  }
+  return sigstore.toDSSEBundle({
+    artifact: artifact.data,
+    artifactType: artifact.type,
+    signature: signature.signature,
+    certificate:
+      signature.key.$case === 'x509Certificate'
+        ? pem.toDER(signature.key.certificate)
+        : undefined,
+    keyHint:
+      signature.key.$case === 'publicKey' ? signature.key.hint : undefined,
+  });
 }
