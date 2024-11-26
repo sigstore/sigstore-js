c2ca1217ced5aa2d27ebc75b2ff85ba3c63aacc9
Brian DeHamer <bdehamer@github.com>
2024-11-26 10:20:34 -0800

    fulcio mock support for all GH claims (#1319)
    Signed-off-by: Brian DeHamer <bdehamer@github.com>
    Co-authored-by: Samuel Giddins <segiddins@segiddins.me>

--- packages/mock/src/fulcio/handler.ts
+++ packages/mock/src/fulcio/handler.ts
@@ -18,14 +18,33 @@ import assert from 'assert';
 import { generateKeyPairSync } from 'crypto';
 import * as jose from 'jose';
 import type { Handler, HandlerFn, HandlerFnResult } from '../shared.types';
-import type { CA } from './ca';
+import type { CA, ExtensionValue } from './ca';
 
 const CREATE_SIGNING_CERT_PATH = '/api/v2/signingCert';
 const DEFAULT_SUBJECT = 'NO-SUBJECT';
 const DEFAULT_ISSUER = 'https://fake.oidcissuer.com';
 
 const ISSUER_EXT_OID_V1 = '1.3.6.1.4.1.57264.1.1';
+const GH_WORKFLOW_TRIGGER_EXT_OID = '1.3.6.1.4.1.57264.1.2';
+const GH_WORKFLOW_SHA_EXT_OID = '1.3.6.1.4.1.57264.1.3';
+const GH_WORKFLOW_NAME_EXT_OID = '1.3.6.1.4.1.57264.1.4';
+const GH_WORKFLOW_REPO_EXT_OID = '1.3.6.1.4.1.57264.1.5';
+const GH_WORKFLOW_REF_EXT_OID = '1.3.6.1.4.1.57264.1.6';
 const ISSUER_EXT_OID_V2 = '1.3.6.1.4.1.57264.1.8';
+const BUILD_SIGNER_URI_EXT_OID = '1.3.6.1.4.1.57264.1.9';
+const BUILD_SIGNER_DIGEST_EXT_OID = '1.3.6.1.4.1.57264.1.10';
+const RUNNER_ENVIRONMENT_EXT_OID = '1.3.6.1.4.1.57264.1.11';
+const SOURCE_REPO_URI_EXT_OID = '1.3.6.1.4.1.57264.1.12';
+const SOURCE_REPO_DIGEST_EXT_OID = '1.3.6.1.4.1.57264.1.13';
+const SOURCE_REPO_REF_EXT_OID = '1.3.6.1.4.1.57264.1.14';
+const SOURCE_REPO_ID_EXT_OID = '1.3.6.1.4.1.57264.1.15';
+const SOURCE_REPO_OWNER_URI_EXT_OID = '1.3.6.1.4.1.57264.1.16';
+const SOURCE_REPO_OWNER_ID_EXT_OID = '1.3.6.1.4.1.57264.1.17';
+const BUILD_CONFIG_URI_EXT_OID = '1.3.6.1.4.1.57264.1.18';
+const BUILD_CONFIG_DIGEST_EXT_OID = '1.3.6.1.4.1.57264.1.19';
+const BUILD_TRIGGER_EXT_OID = '1.3.6.1.4.1.57264.1.20';
+const RUN_INVOCATION_URI_EXT_OID = '1.3.6.1.4.1.57264.1.21';
+const SOURCE_REPO_VISIBILITY_EXT_OID = '1.3.6.1.4.1.57264.1.22';
 
 interface FulcioHandlerOptions {
   strict?: boolean;
@@ -52,18 +71,17 @@ function createSigningCertHandler(
   return async (body: string): Promise<HandlerFnResult> => {
     try {
       // Extract relevant fields from the request
-      const { subject, issuer, publicKey } = strict
+      const { subject, publicKey, claims } = strict
         ? parseBody(body, subjectClaim)
         : stubBody();
+
+      const extensions = extensionFromClaims(claims);
 
       // Request certificate from CA
       const cert = await ca.issueCertificate({
         publicKey: fromPEM(publicKey),
         subjectAltName: subject,
-        extensions: [
-          { oid: ISSUER_EXT_OID_V1, value: issuer, legacy: true },
-          { oid: ISSUER_EXT_OID_V2, value: issuer },
-        ],
+        extensions: extensions,
       });
 
       // Format the response
@@ -80,31 +98,34 @@ function createSigningCertHandler(
 function parseBody(
   body: string,
   subjectClaim: string
-): { subject: string; issuer: string; publicKey: string } {
+): { subject: string; publicKey: string; claims: Record<string, string> } {
   const json = JSON.parse(body.toString());
   const oidc = json.credentials.oidcIdentityToken;
   const pem = json.publicKeyRequest.publicKey.content;
 
   // Decode the JWT
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  const claims = jose.decodeJwt(oidc) as any;
+  const claims = jose.decodeJwt(oidc) as Record<string, string>;
 
   /* istanbul ignore next */
   return {
     subject: claims[subjectClaim] || DEFAULT_SUBJECT,
-    issuer: claims['iss'] || DEFAULT_ISSUER,
     publicKey: pem,
+    claims: { iss: DEFAULT_ISSUER, ...claims },
   };
 }
 
-function stubBody(): { subject: string; issuer: string; publicKey: string } {
+function stubBody(): {
+  subject: string;
+  publicKey: string;
+  claims: Record<string, string>;
+} {
   const { publicKey } = generateKeyPairSync('ec', {
     namedCurve: 'P-256',
   });
   return {
     subject: DEFAULT_SUBJECT,
-    issuer: DEFAULT_ISSUER,
     publicKey: publicKey.export({ format: 'pem', type: 'spki' }).toString(),
+    claims: { iss: DEFAULT_ISSUER },
   };
 }
 
@@ -117,6 +138,132 @@ function buildResponse(leaf: Buffer, root: Buffer): string {
     },
   };
   return JSON.stringify(body);
+}
+
+function extensionFromClaims(claims: Record<string, string>): ExtensionValue[] {
+  const extensions: ExtensionValue[] = [];
+  const baseURL = 'https://github.com';
+
+  for (const [key, value] of Object.entries(claims)) {
+    switch (key) {
+      case 'iss':
+        extensions.push({
+          oid: ISSUER_EXT_OID_V1,
+          value: value,
+          legacy: true,
+        });
+        extensions.push({ oid: ISSUER_EXT_OID_V2, value: value });
+        break;
+      case 'event_name':
+        extensions.push({
+          oid: GH_WORKFLOW_TRIGGER_EXT_OID,
+          value: value,
+          legacy: true,
+        });
+        extensions.push({ oid: BUILD_TRIGGER_EXT_OID, value: value });
+        break;
+      case 'sha':
+        extensions.push({
+          oid: GH_WORKFLOW_SHA_EXT_OID,
+          value: value,
+          legacy: true,
+        });
+        extensions.push({ oid: SOURCE_REPO_DIGEST_EXT_OID, value: value });
+        break;
+      case 'workflow':
+        extensions.push({
+          oid: GH_WORKFLOW_NAME_EXT_OID,
+          value: value,
+          legacy: true,
+        });
+        break;
+      case 'repository':
+        extensions.push({
+          oid: GH_WORKFLOW_REPO_EXT_OID,
+          value: value,
+          legacy: true,
+        });
+        extensions.push({
+          oid: SOURCE_REPO_URI_EXT_OID,
+          value: `${baseURL}/${value}`,
+        });
+        break;
+      case 'ref':
+        extensions.push({
+          oid: GH_WORKFLOW_REF_EXT_OID,
+          value: value,
+          legacy: true,
+        });
+        extensions.push({
+          oid: SOURCE_REPO_REF_EXT_OID,
+          value: value,
+        });
+        break;
+      case 'job_workflow_ref':
+        extensions.push({
+          oid: BUILD_SIGNER_URI_EXT_OID,
+          value: `${baseURL}/${value}`,
+        });
+        break;
+      case 'job_workflow_sha':
+        extensions.push({
+          oid: BUILD_SIGNER_DIGEST_EXT_OID,
+          value: value,
+        });
+        break;
+      case 'runner_environment':
+        extensions.push({
+          oid: RUNNER_ENVIRONMENT_EXT_OID,
+          value: value,
+        });
+        break;
+      case 'repository_id':
+        extensions.push({
+          oid: SOURCE_REPO_ID_EXT_OID,
+          value: value,
+        });
+        break;
+      case 'repository_owner':
+        extensions.push({
+          oid: SOURCE_REPO_OWNER_URI_EXT_OID,
+          value: `${baseURL}/${value}`,
+        });
+        break;
+      case 'repository_owner_id':
+        extensions.push({
+          oid: SOURCE_REPO_OWNER_ID_EXT_OID,
+          value: value,
+        });
+        break;
+      case 'workflow_ref':
+        extensions.push({
+          oid: BUILD_CONFIG_URI_EXT_OID,
+          value: `${baseURL}/${value}`,
+        });
+        break;
+      case 'workflow_sha':
+        extensions.push({
+          oid: BUILD_CONFIG_DIGEST_EXT_OID,
+          value: value,
+        });
+        break;
+      case 'repository_visibility':
+        extensions.push({
+          oid: SOURCE_REPO_VISIBILITY_EXT_OID,
+          value: value,
+        });
+        break;
+    }
+  }
+
+  if (claims['repository'] && claims['run_id'] && claims['run_attempt']) {
+    extensions.push({
+      oid: RUN_INVOCATION_URI_EXT_OID,
+      value: `${baseURL}/${claims['repository']}/actions/runs/${claims['run_id']}/attempts/${claims['run_attempt']}`,
+    });
+  }
+
+  return extensions;
 }
 
 // PEM string to DER-encoded byte buffer conversion
